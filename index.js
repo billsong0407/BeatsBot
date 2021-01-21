c42f959be48ac0fd95fbf5df0cab56d93834d6f0
Bill Song <songchao456@gmail.com>
2021-01-21 15:25:10 -0500

    small fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Collection } = require("discord.js");
-const { readdirSync } = require("fs");
+const { fs } = require("fs");
 const { join } = require("path");
 const { TOKEN, PREFIX } = require("./utility/util");
 
@@ -9,27 +9,24 @@ const client = new Client();
 client.login(TOKEN)
 client.commands = new Collection();
 
-// const botCommands = require('./commands/ping');
-
-// Object.keys(botCommands).map(key => {
-//   client.commands.set(botCommands[key].name, botCommands[key]);
-// });
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () =>{
     console.log(`${client.user.username} ready!`);
     client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
 });
 
-const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
-for (const file of commandFiles) {
-    const command = require(join(__dirname, "commands", `${file}`));
-    client.commands.set(command.name, command);
-}
+client.on('message', async(msg) => {
+  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
 
-client.on('message', msg => {
   const args = msg.content.split(/ +/);
   const command = args.shift().toLowerCase();
   console.info(`Called command: ${command}`);
+  
   if (!client.commands.has(command)) return;
 
   try {
@@ -39,7 +36,3 @@ client.on('message', msg => {
     msg.reply('there was an error trying to execute that command!');
   }
 });
-
-/**
- * Import all commands
- */
