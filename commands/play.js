e2cfbcb12a8a67ac5249ab475f005b418bc13b1e
Bill Song <songchao456@gmail.com>
2021-01-21 17:37:31 -0500

    major fix on play function

--- commands/play.js
+++ commands/play.js
@@ -1,16 +1,45 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
+const { DEFAULT_VOLUME } = require("../utility/util");
+
+// let { queue } = require('../utility/queue');
 
 module.exports = {
-    name: "play",
+    name: "$play",
     description: "plays music",
-    execute(msg, args) {
+    async execute(msg, args) {
         const voice_channel = msg.member.voice.channel;
+        const serverQueue = msg.client.queue.get(msg.guild.id);
+        const queueConstruct = {
+            textChannel: msg.channel,
+            channel: msg.member.voice.channel,
+            connection: null,
+            songs: [],
+            loop: false,
+            volume: DEFAULT_VOLUME || 100,
+            playing: true
+        };
+        url = args[0];
+
+        function play(msg){
+            console.info("playing");
+            var server = msg.client.queue.get(msg.guild.id);
+            console.info(server.songs[0]);
+            server.dispatcher = server.connection.play(ytdl(server.songs[0].url, {filter: "audioonly"}));
+            server.songs.shift();
+            server.dispatcher.on("end", function(){
+                if (servers.songs[0]){
+                    play(msg);
+                }else{
+                    queueConstruct.connection.disconnect();
+                }
+            })
+        }
 
         if (!voice_channel) {
             return msg.reply("You need to join a voice channel first!");
         }
-        const permission_list = voice_channel.permissionFor(message.client.user);
+        const permission_list = msg.member.voice.channel.permissionsFor(msg.client.user);
 
         if (!permission_list.has("CONNECT") || !permission_list.has("SPEAK")){
             return msg.reply("You do not have the correct permissions to use it")
@@ -20,11 +49,47 @@ module.exports = {
             return msg.channel.send("You need to enter a keyword of http link")
         }
 
+        let songInfo = null;
+        let song = null;
+
+        try {
+            songInfo = await ytdl.getInfo(url);
+            song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url,
+                duration: songInfo.videoDetails.lengthSeconds
+            };
+        } catch (error) {
+            console.error(error);
+            return msg.reply(error.msg).catch(console.error);
+        }
+
+        if (serverQueue) {
+            serverQueue.songs.push(song);
+            return serverQueue.textChannel
+            .send(`✅ **${song.title}** has been added to the queue by ${msg.author}`)
+            .catch(console.error);
+        }
+
+        queueConstruct.songs.push(song);
+        msg.client.queue.set(msg.guild.id, queueConstruct);
+
+        try {
+            queueConstruct.connection = await voice_channel.join();
+            await queueConstruct.connection.voice.setSelfDeaf(false);
+            play(msg);
+        } catch (error) {
+            console.error(error);
+            msg.client.queue.delete(msg.guild.id);
+            await channel.leave();
+            return msg.channel.send(`Could not join the channel: ${error}`).catch(console.error);
+        }
+
         
 
         // const connection = await voice_channel.join();
         // const videoFinder = async(query) =>{
         //     const video_result
         // }
-    }
-}
+    },
+};
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = new Client();
 
 client.login(TOKEN)
 client.commands = new Collection();
+client.queue = new Map();
 
 const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
@@ -27,41 +28,13 @@ client.on('message', async(msg) => {
   if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
 
   const args = msg.content.split(/ +/);
-  // const args = msg.content.substring(PREFIX.length).split(" ");
   const command = args.shift().toLowerCase();
   console.info(`Called command: ${command}`);
   
-  // if (!client.commands.has(command)) return;
-  console.info(args);
-  try {
-    if (command == '$play'){
-      if (!args[0]){
-        return msg.channel.send("you need to join a channel first");
-      }
-      
-      if(!servers[msg.guild.id]) servers[msg.guild.id] = {
-        queue: []
-      }
-      
-      var server = servers[msg.guild.id];
-      server.queue.push(args[0])
+  if (!client.commands.has(command)) return;
 
-      if (!msg.guild.voiceConnection) msg.member.voice.channel.join().then(function(connection){
-          // play(connection, msg);
-        var server = servers[msg.guild.id];
-        server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audioonly"}));
-        server.queue.shift();
-        server.dispatcher.on("end", function(){
-          if (server.queue[0]){
-            play(connection, msg);
-          }else{
-            connection.disconnect();
-          }
-        })
-      })
-    }else{
-      client.commands.get(command).execute(msg, args);
-    }
+  try {
+    client.commands.get(command).execute(msg, args);
   } catch (error) {
     console.error(error);
     msg.reply('there was an error trying to execute that command!');
