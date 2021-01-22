5326a804138a9410822e94dab7ca9125122adeac
Bill Song <songchao456@gmail.com>
2021-01-22 18:48:42 -0500

    add stop command

--- commands/stop.js
+++ commands/stop.js
@@ -1,15 +1,14 @@
 module.exports = {
-    name: 'skip',
-    description: 'skip a song',
+    name: 'stop',
+    description: 'stop playing music',
     execute(msg, args){
         const current_server = msg.client.queue.get(msg.guild.id);
         if (!current_server || !current_server.connection.dispatcher){
             msg.reply("Currently no music playing!");
         }else{
-            song = current_server.waiting_list[0];
+            current_server.waiting_list = [];
             current_server.connection.dispatcher.end();
-            current_server.waiting_list.shift();
-            current_server.text_channel.send(`${msg.author} skipped ${song.title}`);
+            current_server.text_channel.send(`${msg.author} stopped playing music!`);
         }
         return;
     },
