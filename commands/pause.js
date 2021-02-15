fd98debd35308c0da4d66f38eaf5f536dc7f5190
Bill Song <songchao456@gmail.com>
2021-02-15 16:52:31 -0500

    add pause command

---
+++ commands/pause.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "pause",
+  description: "Pause the currently playing song",
+  execute(message) {
+    const current_server = message.client.servers.get(message.guild.id);
+    if (!current_server) return message.reply("No songs are currently playing.").catch(console.error);
+
+    if (current_server.playing) {
+      current_server.playing = false;
+      current_server.connection.dispatcher.pause(true);
+      return current_server.text_channel.send(`${message.author} ⏸ paused the music.`).catch(console.error);
+    }
+  }
+};
