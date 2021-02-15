91e1ad836b38a7624e370c90efc912360bca9cb6
Bill Song <songchao456@gmail.com>
2021-02-15 16:14:32 -0500

    fix bug

--- commands/skip.js
+++ commands/skip.js
@@ -2,13 +2,12 @@ module.exports = {
     name: 'skip',
     description: 'skip a song',
     execute(msg, args){
-        const current_server = msg.client.queue.get(msg.guild.id);
+        const current_server = msg.client.servers.get(msg.guild.id);
         if (!current_server || !current_server.connection.dispatcher){
             msg.reply("Currently no music playing!");
         }else{
             song = current_server.waiting_list[0];
             current_server.connection.dispatcher.end();
-            current_server.waiting_list.shift();
             current_server.text_channel.send(`${msg.author} skipped ${song.title}`);
         }
         return;
