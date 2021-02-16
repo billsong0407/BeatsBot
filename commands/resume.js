91d5f32179cb6df3982cde5767d5d8c93c39f7cc
Bill Song <songchao456@gmail.com>
2021-02-16 14:14:14 -0500

    fix bug

--- commands/resume.js
+++ commands/resume.js
@@ -4,7 +4,7 @@ module.exports = {
   execute(message) {
     const current_server = message.client.servers.get(message.guild.id);
     if (!current_server) return message.reply("There is nothing playing.").catch(console.error);
-    if (message.member.voice != message.member.guild.voice.channelID) return message.reply("You must join a voice channel first");
+    if (message.member.voice.channelID != message.member.guild.voice.channelID) return message.reply("You must join a voice channel first");
 
     if (!current_server.playing) {
       current_server.playing = true;
