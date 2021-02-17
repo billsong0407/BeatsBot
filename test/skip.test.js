97d25e500f3dad646dfab54fff8c111323cc5d43
Bill Song <songchao456@gmail.com>
2021-02-17 18:12:49 -0500

    add test cases for skip command

--- test/skip.test.js
+++ test/skip.test.js
@@ -1,47 +1,41 @@
-const { Client, TextChannel, Message, Member, Guild, Connection } = require("./mock");
-const { create_queue } = require('../utility/queue');
-const { create_server } = require('./mock_server');
+const { Client, TextChannel, Message, Member, Guild } = require("./mock/mock_general");
+const { create_song } = require('./mock/mock_song');
+const { create_server } = require('./mock/mock_server');
 const skip = require('../commands/skip').execute
 
-test('send', ()=>{
-    const msg = Message;
-    const channel = TextChannel;
-    channel.guild = 1234;
-    const member = Member;
-    msg.channel = channel;
-    msg.member = member;
-    member.voice.channel = channel;
-    server = create_queue(msg);
-    server.connection = Connection;
-    server.waiting_list.push("Jingle Bell");
-    server.waiting_list.push("Frozen");
-    const client = Client;
-    client.servers.set(1234,server);
-    msg.client = client;
-    msg.guild = Guild;
-    skip(msg);
-    expect(client.servers.get(1234).waiting_list).toStrictEqual(["Jingle Bell", "Frozen"]);
-    // skip(msg);
-    // expect(client.servers.get(1234).waiting_list).toStrictEqual(["Frozen"]);
-})
+describe('test skip command', ()=>{
 
-test('send1', ()=>{
-    const msg = Message;
-    const channel = TextChannel;
-    channel.guild = 1234;
-    const member = Member;
-    msg.channel = channel;
-    msg.member = member;
-    member.voice.channel = channel;
-    server = create_server(msg);
-    server.waiting_list.push("Jingle Bell");
-    server.waiting_list.push("Frozen");
-    const client = Client;
-    client.servers.set(1234,server);
-    msg.client = client;
-    msg.guild = Guild;
-    skip(msg);
-    expect(client.servers.get(1234).waiting_list).toStrictEqual(["Frozen"]);
-    // skip(msg);
-    // expect(client.servers.get(1234).waiting_list).toStrictEqual(["Frozen"]);
+    let msg = Message;
+    let client = Client;
+    let channel = TextChannel;
+    let member = Member;
+
+    beforeEach(() =>{
+        channel.guild = 1234;
+        msg.channel = channel;
+        msg.member = member;
+        member.voice.channel = channel;
+        server = create_server(msg);
+        song1 = create_song("Jingle Bell", "h1", 30);
+        song2 = create_song("Frozen", "h1", 30);
+        server.waiting_list.push(song1);
+        server.waiting_list.push(song2);
+        client.servers.set(1234,server);
+        msg.client = client;
+        msg.guild = Guild;
+    });
+
+    test('skip 1 song', ()=>{
+        skip(msg);
+        expect(client.servers.get(1234).waiting_list.length).toBe(1);
+        expect(client.servers.get(1234).waiting_list[0].title).toBe("Frozen");
+        expect(channel.content).toBe("TestUser1 skipped Jingle Bell");
+    });
+    test('skip 2 songs', ()=>{
+        skip(msg);
+        expect(channel.content).toBe("TestUser1 skipped Jingle Bell");
+        skip(msg);
+        expect(channel.content).toBe("TestUser1 skipped Frozen");
+        expect(client.servers.get(1234).waiting_list.length).toBe(0);
+    });
 })
