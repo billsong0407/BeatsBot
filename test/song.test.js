b0cb5745eada47cb2e5c838e0117bbe76d01f262
Bill Song <songchao456@gmail.com>
2021-03-21 14:28:49 -0400

    update song test suites

--- test/song.test.js
+++ test/song.test.js
@@ -1,16 +1,32 @@
 const { create_song } = require('../utility/song');
 
-test("test song object construction", () => {
+describe("test song object constuction", () => {
     const videoDetails ={
         title: "test",
         video_url: "https://www.test.com",
         lengthSeconds: 100,
         artist: null,
+        viewCount: 10,
+        averageRating: 9,
+        author: {
+            name: "bot123",
+        },
     }
-    const song_info = {};
-    song_info.videoDetails = videoDetails;
-    song = create_song(song_info, "Test123");
-    expect(song.title).toBe("test");
-    expect(song.url).toBe("https://www.test.com");
-    expect(song.duration).toBe(100);
+    test("Functional test #1", () => {
+        const song_info = {};
+        song_info.videoDetails = videoDetails;
+        song = create_song(song_info, "Test123");
+        expect(song.title).toBe("test");
+        expect(song.url).toBe("https://www.test.com");
+        expect(song.duration).toBe(100);
+    });
+
+    test("Functional test #2", () => {
+        const song_info = {};
+        song_info.videoDetails = videoDetails;
+        song = create_song(song_info, "Test123");
+        expect(song.artist).toBe("bot123");
+        expect(song.requested_by).toBe("Test123");
+        expect(song.view_count).toBe(10);
+    });
 });
