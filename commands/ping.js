df381821ecdffc81fa2376bdd86671d669700e32
Bill Song <songchao456@gmail.com>
2021-03-29 21:10:13 -0400

    Add ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
-  name: 'ping',
-  description: 'Ping',
-  execute(msg, args) {
-    msg.channel.send('pong');
-  },
+  name: "ping",
+  cooldown: 10,
+  description: "Reveal the bot's average ping",
+  execute(message) {
+    message.reply(`📈 Average ping to API: ${Math.round(message.client.ws.ping)} ms`).catch(console.error);
+  }
 };
